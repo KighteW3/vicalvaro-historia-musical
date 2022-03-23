090bd1d06e3adaa5e5048d0b997015157449a226
kighte <kighte04@gmail.com>
2022-03-23 19:51:10 +0100

    pasaron cositas jaja

---
+++ code.js
@@ -0,0 +1,11 @@
+/* const feaE = document.getElementById("pablo__casado");
+
+let pabloText = feaE.textContent;
+
+let lenghtNumber = 0;
+
+for (i = 0; i < pabloText.length; i++) {
+    lenghtNumber++;
+}
+
+alert(lenghtNumber); */
---
+++ pee/code.js
@@ -0,0 +1,5 @@
+const pee = document.querySelector(".titulo");
+
+let pepa = pee.textContent;
+
+document.write(pepa);
